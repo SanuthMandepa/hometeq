f5396e0670a1205448cd40661e92265bc496bf3c
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-04-17 12:59:43 +0530

    Task 3

--- prodbuy.php
+++ prodbuy.php
@@ -11,64 +11,37 @@ echo "<h4>".$pagename."</h4>"; //display name of the page on the web page
 //store the value in a local variable called $prodid
 $prodid=$_GET['u_prod_id'];
 //display the value of the product id, for debugging purposes
-echo "<p>Selected product Id: ".$prodid;
-$SQL="select prodId, prodName, prodPicNameLarge,prodDescripLong,prodPrice,prodQuantity from Product WHERE prodId = $prodid";
-//run SQL query for connected DB or exit and display error message
-$exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error($conn));
+//echo "<p>Selected product Id: ".$prodid;
+//create a $SQL variable and populate it with a SQL statement that retrieves details for the selected product
+$SQL="select prodId, prodName, prodPicNameLarge, prodDescripLong, prodPrice, prodQuantity 
+from Product
+where prodId=".$prodid;
+$exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error());
+$arrayp=mysqli_fetch_array($exeSQL);
 echo "<table style='border: 0px'>";
-//create an array of records (2 dimensional variable) called $arrayp.
-//populate it with the records retrieved by the SQL query previously executed. 
-//Iterate through the array i.e while the end of the array has not been reached, run through it
-while ($arrayp=mysqli_fetch_array($exeSQL))
-{
-    echo "<tr>";
-    echo "<td style='border: 0px'>";
-    //display the small image whose name is contained in the array
-
-    //make the image into an anchor to prodbuy.php and pass the product id by URL (the id from the array)
-    echo "<a href='prodbuy.php?u_prod_id=".$arrayp['prodId']."'>";
-
-    echo "<img src='images/".$arrayp['prodPicNameLarge']."' height='200' width='200'>";
-    //close the anchor
-    echo "</a>";
-    echo "</td>";
-    echo "<td style='border: 0px'>";
-    echo "<p><h5>".$arrayp['prodName']."</h5>"; //display product name as contained in the array
-    echo "<p>".$arrayp['prodDescripLong'];
-    echo "<p><b>&pound;".$arrayp['prodPrice']."</b>"; // Added missing semicolon after "&pound"
-    echo "<p>".$arrayp['prodQuantity'];
-
-    echo "</td>";
-    echo "</tr>";
-    echo "<p>Number to be purchased: ";
-
-// Fetch the stock level for the product from the database
-//$stockLevel = $arrayp['prodQuantity'];
-
-// Create the drop-down list
-echo "<form action='basket.php' method='post'>";
-echo "<select name='p_quantity'>";
-
-// Loop to generate options in the drop-down list
-for ($i = 1; $i <= $arrayp['prodQuantity']; $i++) {
-    echo "<option value='".$i."'>".$i."</option>";
-}
-
-// Close the drop-down list
-echo "</select>";
-
-//create form made of one text field and one button for user to enter quantity
-//the value entered in the form will be posted to the basket.php to be processed
-
-echo "<input type='submit name='submitbtn' value='ADD TO BASKET' id='submitbtn'>";
-//pass the product id to the next page basket.php as a hidden value
-echo "<input type='hidden' name='h_prodid' value='".$prodid."'>";
-echo "</form>";
-echo "</p>";
-
-}
+echo "<tr>";
+echo "<td style='border: 0px'>";
+echo "<img src=images/".$arrayp['prodPicNameLarge']." height=350 width=350>";
+echo "</td>";
+echo "<td style='border: 0px'>";
+echo "<p><h5>".$arrayp['prodName']."</h5></p>";
+echo "<br><p>".$arrayp['prodDescripLong']."</p>";
+echo "<br><p><b>&pound".$arrayp['prodPrice']."</b></p>";
+echo "<br><p>Number left in stock: ".$arrayp['prodQuantity'] ."</p>";
+echo "</td>";
+echo "</tr>"; 
 echo "</table>";
 
+echo "<br><p>Number to be purchased: ";
+//create form made of one text field and one button for user to enter quantity
+//the value entered in the form will be posted to the basket.php to be processed
+echo "<form action=basket.php method=post>";
+echo "<input type=text name=p_quantity size=5 maxlength=3>";
+echo "<input type=submit name='submitbtn' value='ADD TO BASKET' id='submitbtn'>";
+//pass the product id to the next page basket.php as a hidden value
+echo "<input type=hidden name=h_prodid value=".$prodid.">";
+echo "</form>";
+echo "</p>";
 
 //display random text
 
