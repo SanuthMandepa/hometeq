bf2254f4b9ce50f050c30688b6f76167e22f1362
SanuthMandepa <dssanuthmandepa@gmail.com>
2024-04-17 16:30:31 +0530

    table new

--- basket.php
+++ basket.php
@@ -33,6 +33,51 @@ else
 echo "<p>Basket unchanged";
 }
 
+$total= 0; //Create a variable $total and initialize it to zero
+//Create HTML table with header to display the content of the basket: prod name, price, selected quantity and subtotal
+echo "<p><table id='baskettable'>";
+echo "<tr>";
+echo "<th>Product Name</th><th>Price</th><th>Quantity</th><th>Subtotal</th>";
+echo "</tr>";
+//if the session array $_SESSION['basket'] is set
+if (isset($_SESSION['basket']))
+{
+ //loop through the basket session array for each data item inside the session using a foreach loop 
+ //to split the session array between the index and the content of the cell
+ //for each iteration of the loop
+ //store the id in a local variable $index & store the required quantity into a local variable $value
+ foreach($_SESSION['basket'] as $index => $value)
+ {
+ //SQL query to retrieve from Product table details of selected product for which id matches $index
+ //execute query and create array of records $arrayp
+ $SQL="select prodId, prodName, prodPrice from Product where prodId=".$index;
+ $exeSQL=mysqli_query($conn, $SQL) or die (mysql_error());
+ $arrayp=mysqli_fetch_array($exeSQL);
+ echo "<tr>";
+ //display product name & product price using array of records $arrayp
+ echo "<td>".$arrayp['prodName']."</td>";
+ echo "<td>&pound".number_format($arrayp['prodPrice'],2)."</td>";
+ // display selected quantity of product retrieved from the cell of session array and now in $value
+ echo "<td style='text-align:center;'>".$value."</td>";
+ //calculate subtotal, store it in a local variable $subtotal and display it
+ $subtotal=$arrayp['prodPrice'] * $value;
+ echo "<td>&pound".number_format($subtotal,2)."</td>";
+ echo "</tr>";
+ //increase total by adding the subtotal to the current total
+ $total=$total+$subtotal;
+ }
+}
+//else display empty basket message
+else 
+{
+echo "<p>Empty basket";
+}
+// Display total
+echo "<tr>";
+echo "<td colspan=3>TOTAL</td>";
+echo "<td>&pound".number_format($total,2)."</td>";
+echo "</tr>";
+echo "</table>";
 
 // //if the posted ID of the new product is set i.e. if the user is adding a new product into the basket
 // if (isset($_POST['h_prodid']))
